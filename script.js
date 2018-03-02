3d3d71b1ec2df619de08f76158275ea62d340e91
Rachel Bengochea <rachel.bengochea@byu.net>
2018-03-02 01:09:41 -0700

    Finished

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ var app = new Vue({
     },
     size: function(number){
       this.difficulty = number;
+      this.loadGame();
       this.loadSquares();
     },
     inMines: function(number) {
@@ -46,20 +47,10 @@ var app = new Vue({
         this.flagged.splice(i, 1);
       }
     },
-    checkforWin: function(){
-      if(this.flagged.length !== this.mines.length){
-        return false;
-      }
-      for(i = 0; i < this.flagged.length; i++){
-        if(!this.inMines(this.flagged[i])){
-          return false;
-        }
-      }
-      return true;
-    },
     loadGame: function(){
       this.squares = [];
       this.mines= [];
+      this.flagged = [];
    },
    getSize(){
      if (this.difficulty === 0){
@@ -72,30 +63,37 @@ var app = new Vue({
    },
     loadSquares: function() {
       this.getSize();
+      if(this.numofMines > this.numSquares){
+        this.numofMines = this.numSquares;
+      }
+      if(this.numofMines < 1){
+        this.numofMines = 1;
+      }
       for(i = 0; i < this.numSquares; i++){
        this.squares.push({class:true, id: i, value: ""});
      }
-     console.log(this.squares);
      for(i = 0; i < this.numofMines; i++){
+       var rand = this.randomNumber();
+       while(this.inMines(rand)){
+         rand = this.randomNumber();
+       }
        this.mines.push(this.randomNumber());
+       this.flagged.push(this.randomNumber());
      }
-     console.log(this.mines);
     },
     endGame: function() {
       this.message = "Play Again!";
       this.loadGame();
-      this.loadGame();
     },
     flagSquare: function(square) {
-      if(this.squares[square].value !== "#"){
-        this.squares[square].value = "#";
-        this.flagged.push(square);
-      }else{
-        this.squares[square].value = "";
-        this.squares[square].class = true;
-        this.removeFlag(square);
-      }
-      if(this.checkforWin()){
+      this.squares[square].value = "#";
+      this.squares[square].class = false;
+      this.flagged = this.flagged.filter(function(item) {
+        var equal = item === square;
+        return !equal;
+      });ß
+      if(this.flagged.length === 0){
+        alert("You're the Winner!");
         this.endGame();
       }
     },
@@ -103,6 +101,9 @@ var app = new Vue({
       if(this.squares[square].value === "#"){
         this.squares[square].value = "";
         this.squares[square].class = true;
+        if(this.inMines(square)){
+          this.flagged.push(square);
+        }
         return;
       }
       this.squares[square].class = false;
